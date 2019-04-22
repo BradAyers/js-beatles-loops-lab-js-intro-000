323bb821fb77c04d74d115cf6f3a67df7cb29796
Brad Ayers <brad@bradayers.com>
2019-04-22 03:35:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var whoPlaysWhat = [];
+  for (i = 0; i < musicians.length; i++) {
+    whoPlaysWhat = [...whoPlaysWhat, `${musicians} plays ${instruments}`];
+  }
+  return whoPlaysWhat;
+}
